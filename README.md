ff6f4aba7ac81f42e1d97029d8268c430d228f46
Jason Koo <jalakoo@gmail.com>
2020-03-30 21:47:41 -0700

    Default display simplified w/ advanced options triggered by checkboxes

--- README.md
+++ README.md
@@ -2,32 +2,17 @@
 This is a simple cost of meetings calculator based on annual salaries for selected attendees. The app itself does not account for regional salary differences, taxes, or other operational costs. These however, could be included in your own custom dataset.
 
 # OPTIONS
-## Salary Data URL
-Defaults to a dataset of average annual job salaries for a very limited set of roles in the US, at the date of this commit. An alternate address to a static .csv file may be used, so long as the .csv file contains these column names: `NAME,ANNUAL_SALARY` and the `ANNUAL_SALARY` column contains only integer representations of a positions annual salary (no currency indicators, commas or decimal values). For example: `
-```
-NAME,ANNUAL_SALARY
-Full Stack Developer - Junior,93000
-```
-A re-indexed version of this .csv will display in the `Salaries` table.
-
-*NOTE: Currency sign was intentionally omitted so a dataset with any currency value could be used.*
-
-## Choose attendee job roles
-Select the name of the attendees' positions to add them to the cost calculator.
-
-*NOTE: Currently only one of each job role can be selected from the example data set. Use a different dataset with name variations having the same salary value or something like an employee-id to differentiate roles.* 
-
-## Lefthand Panel
-Contains additional cost calculation parameters.
-
-### Meeting Length
-How long the meeting is in minutes. Currently 480min or 8hrs is the arbitrarily set max calculable time.
-
-### Repeating Occurances Per Month
-How many times a month this particular meeting occurs. Set 0 if it's a one-off meeting. Max is 20, which would be every work day for a typical 4 week month.
-
-### Working Months
-How many months out of the year to consider in the calculation. Default is sit to 11 months to account for 2 weeks of set holidays, and another 2 to cover time off, sicknesses, and other events. Maybe useful for calculating seasonal or limited length projects. Setting this to 0 will also equate calculation to a one-off meeting.
+FIELD NAME | DESCRIPTION | DEFAULT VALUE
+----- | ----- | --------
+Currency Symbol | For indicating which currency for displaying cost information | $
+Meeting Length | Length of a meeting in minutes | 60
+Occurrences Per Month | How often in a month a meeting repeats. 20 equates to every business day over a 4 week period. Set to 0 if the meeting is not recurring | 4
+Working Months | How many months that this meeting will occur over | 11
+Salary Data Source URL | Defaults to a dataset of average annual job salaries for a very limited set of roles in the US, at the date of this commit. An alternate address to a static .csv file may be used, so long as the .csv file contains these column names: `NAME,ANNUAL_SALARY`. The `ANNUAL_SALARY` column contains only integer representations of a positions annual salary (no currency indicators, commas or decimal values). A re-indexed version of this .csv will display in the `Salary Data` table. | Sample data from an AWS S3 bucket is provided
+Meeting Attendees | Multiselect list of attendee by job roles (if using the provided default data source) | 4 developer jobs roles
+Edit Data Source | Checkbox for displaying Salary Data Source URL field | Unchecked
+Show Salary Data | Checkbox for displaying Indexed Salary data from the data source | Unchecked
+Show Cost Graph | Checkbox for displaying a graph of monthly aggregating cost over time | Unchecked
 
 ## REQUIREMENTS
 ### STREAMLIT
--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 # TITLE
 st.title("Meetings Cost Calculator")
-st.markdown('<font color=\"grey\">*This mini application displays the total cost for a given instance of a meeting and optionally, the total annual cost for regularly repeating meetings. For more information and how to run your own local copy of this app, see [this Github repo](https://github.com/jalakoo/bloodymeetings).*</font>', unsafe_allow_html=True)
+st.markdown('<font color=\"grey\">*This app displays the cost of a single meeting and the annual cost of recurring meetings based on attendee salaries. For more information & to run your own local copy, see [this Github repo](https://github.com/jalakoo/bloodymeetings).*</font>', unsafe_allow_html=True)
 st.markdown('<br>', unsafe_allow_html=True)
 
 
@@ -19,22 +19,25 @@ length = st.sidebar.slider("", 15, 480, 60, 15)
 
 ## Occurences of meeting(s)
 st.sidebar.markdown("**Occurences Per Month**:<br>*Set to 0 for a one-off meeting*", unsafe_allow_html=True)
-occurences = st.sidebar.slider("", 0, 20, 4, 1)
+occurrences = st.sidebar.slider("", 0, 20, 4, 1)
 
 ## Working months
 st.sidebar.markdown("**Working Months**:<br>*Adjust for seasonal, or project based options. Default is 11 to account for holidays, time-off, and other events.*", unsafe_allow_html=True)
 working_months = st.sidebar.slider("", 1, 12, 11, 1)
 
-## Display raw data
+## Display Options
+display_data_source = st.sidebar.checkbox('Edit Data Source')
 display_salary_data = st.sidebar.checkbox('Show Salary Data')
-
+display_cost_graph = st.sidebar.checkbox('Show Cost Graph')
 
 # MAIN BODY
 # ============================================
-# DATA SOURCE
+# DATA SOURCE (optional)
 SALARY_COLUMN = 'ANNUAL_SALARY'
-st.markdown('**Salary Data Source URL**:<br>*Can use any static location for a .csv file with the following header columns: **name, annual_salary** *', unsafe_allow_html=True)
-data_url = st.text_input("", "https://bloodymeetings.s3-us-west-1.amazonaws.com/us_annual_salaries.csv")
+data_url = "https://bloodymeetings.s3-us-west-1.amazonaws.com/us_annual_salaries.csv"
+if display_data_source:
+    st.markdown('**Salary Data Source URL**:<br>*Can use any static location for a .csv file with the following header columns: **name, annual_salary** *', unsafe_allow_html=True)
+    data_url = st.text_input("", data_url)
 
 @st.cache(suppress_st_warning=True, persist=True)
 def load_data(url):
@@ -42,7 +45,6 @@ def load_data(url):
     lowercase = lambda x: str(x).lower()
     data.rename(lowercase, axis='columns', inplace=True)
     return data
-    # return data.set_index(['name'])
 raw_data = load_data(data_url)
 data = raw_data.set_index(['name'])
 
@@ -52,22 +54,37 @@ if display_salary_data:
     st.write("", raw_data)
 
 # ATTENDEES - Sample provided
-st.markdown('<br>', unsafe_allow_html=True)
-st.markdown('<br>', unsafe_allow_html=True)
-st.markdown('**Meeting Attendees**:<br>*Select from the following dropdown list of job names. These are drawn from the data source\'s **name** column. NOTE: Multiple instances of a name aren\'t supported.*', unsafe_allow_html=True)
+st.markdown('**Meeting Attendees**:<br>*Select job names from the following dropdown. Only one of each name is currently supported.*', unsafe_allow_html=True)
 jobs = st.multiselect("", list(data.index), ["Software Engineer - Junior", "Software Engineer", "Software Engineer - Senior", "Software Engineering Manager"])
 
 # COST SUMMARY
 # ============================================
-# st.subheader('Cost of a meeting with individuals with the selected names')
-st.markdown('---')
+# Calculate
 combined_annual_cost = 0
 for role in jobs:
     salary = int(data.loc[role, "annual_salary"])
     combined_annual_cost = combined_annual_cost + salary
 cost = (combined_annual_cost/1920) * (length/60)
 cost_rounded = round(cost, 2)
-repeating_cost = cost * (occurences * working_months)
+repeating_cost = cost * (occurrences * working_months)
 repeating_cost_rounded = round(repeating_cost, 2)
 costs = "<pre>Single Meeting Cost: <font color=\'green\'><b>{}{:,}</b></font>   |   Annual Meetings Cost: <font color=\'red\'><b>{}{:,}</b></font></pre>".format(currency_symbol, cost_rounded, currency_symbol, repeating_cost_rounded)
+
+# Create data for graph
+if display_cost_graph:
+    graph_df = pd.DataFrame(columns=['Month','Cost'])
+    for i in range(working_months):
+        month = i + 1
+        month_cost = round((month * occurrences * cost), 2)
+        if month < 10:
+            month_string = 'Month 0{}'.format(month)
+        else:
+            month_string = 'Month {}'.format(month)
+        graph_df = graph_df.append({'Month': month_string,'Cost':month_cost }, ignore_index=True)
+    graph_df = graph_df.set_index(['Month'])
+
+# Display
+st.markdown('---')
+if display_cost_graph:
+    st.area_chart(graph_df, width=12)
 st.markdown(costs, unsafe_allow_html=True)
